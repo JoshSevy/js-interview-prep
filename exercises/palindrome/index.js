731d3d373aad35e146608fef3dc62a2183dda8fe
JoshSevy <joshuasevy@outlook.com>
2020-11-25 18:56:05 -0700

    Add another solution to palindrom

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,9 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('');
+//   return reversed === str;
+// }
+
 function palindrome(str) {
-  const reversed = str.split('').reverse().join('')
-  return reversed === str
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  });
 }
 
 module.exports = palindrome;
