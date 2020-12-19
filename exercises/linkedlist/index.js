9ccf7cc141a159d1ac07802cf6006c2864909d73
JoshSevy <joshuasevy@outlook.com>
2020-12-19 02:39:16 -0700

    Working through linked list problems

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,44 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+  this.head = null;
+  }
+
+  insertFirst(data) {
+    let currentHead = this.head
+    if(!currentHead) {
+      this.head = new Node(data);
+    } else {
+      this.head = new Node(data, currentHead);
+    }
+  }
+
+  size() {
+    let count = 0;
+    let currentHead = this.head;
+    while (currentHead !== null) {
+      count++;
+      currentHead = currentHead.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+
+  }
+}
 
 module.exports = { Node, LinkedList };
