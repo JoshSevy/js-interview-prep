c771b7c9f9432407190894d70c7cc583a3d9ab36
JoshSevy <joshuasevy@outlook.com>
2020-12-31 13:44:18 -0700

    Add remove method to double queue problem

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -20,8 +20,22 @@ class Queue {
     this.second = new Stack();
   }
 
-  add(element) {
+   add(element) {
     this.first.push(element);
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.pop();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
   }
 
 
