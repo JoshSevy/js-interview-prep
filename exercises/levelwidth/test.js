e0156d0c7454c7ec593b3a9365755749fc68a5b2
JoshSevy <joshuasevy@outlook.com>
2021-01-20 19:50:12 -0700

    Passing all tests on levelwidth problem

--- exercises/levelwidth/test.js
+++ exercises/levelwidth/test.js
@@ -1,11 +1,11 @@
 const Node = require('./node');
 const levelWidth = require('./index');
 
-describe.skip('Level Width', () => {
+describe('Level Width', () => {
   it('levelWidth is a function', () => {
     expect(typeof levelWidth).toEqual('function');
   });
-  
+
   it('levelWidth returns number of nodes at widest point', () => {
     const root = new Node(0);
     root.add(1);
@@ -13,17 +13,17 @@ describe.skip('Level Width', () => {
     root.add(3);
     root.children[0].add(4);
     root.children[2].add(5);
-  
+
     expect(levelWidth(root)).toEqual([1, 3, 2]);
   });
-  
+
   it('levelWidth returns number of nodes at widest point', () => {
     const root = new Node(0);
     root.add(1);
     root.children[0].add(2);
     root.children[0].add(3);
     root.children[0].children[0].add(4);
-  
+
     expect(levelWidth(root)).toEqual([1, 1, 2, 1]);
   });
 })
