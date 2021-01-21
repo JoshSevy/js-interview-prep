6da9597b9983e09025b842518366cf95f13b9645
JoshSevy <joshuasevy@outlook.com>
2021-01-20 19:22:05 -0700

    Three tests passing on tree class

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -32,6 +32,17 @@ class Tree {
     this.root = null;
 
   }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
 }
 
 module.exports = { Tree, Node };
