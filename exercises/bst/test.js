9ea75c0aa3f96b41a825f6ec10afd308c552713a
JoshSevy <joshuasevy@outlook.com>
2021-01-21 12:27:30 -0700

    Test passing for insert on binary search tree

--- exercises/bst/test.js
+++ exercises/bst/test.js
@@ -1,21 +1,21 @@
 const Node = require('./index');
 
-describe.skip('BST', () => {
+describe('BST', () => {
   it('Node is a constructor', () => {
     expect(typeof Node.prototype.constructor).toEqual('function');
   });
-  
+
   it('Node can insert correctly', () => {
     const node = new Node(10);
     node.insert(5);
     node.insert(15);
     node.insert(17);
-  
+
     expect(node.left.data).toEqual(5);
     expect(node.right.data).toEqual(15);
     expect(node.right.right.data).toEqual(17);
   });
-  
+
   it('Contains returns node with the same data', () => {
     const node = new Node(10);
     node.insert(5);
@@ -24,11 +24,11 @@ describe.skip('BST', () => {
     node.insert(0);
     node.insert(-5);
     node.insert(3);
-  
+
     const three = node.left.left.right;
     expect(node.contains(3)).toEqual(three);
   });
-  
+
   it('Contains returns null if value not found', () => {
     const node = new Node(10);
     node.insert(5);
@@ -37,7 +37,7 @@ describe.skip('BST', () => {
     node.insert(0);
     node.insert(-5);
     node.insert(3);
-  
+
     expect(node.contains(9999)).toEqual(null);
   });
 
