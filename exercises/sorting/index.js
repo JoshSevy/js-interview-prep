f5b1512492dbc0fcd7d5b6c356241415fa69f999
JoshSevy <joshuasevy@outlook.com>
2021-03-08 22:14:45 -0700

    Implement merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -20,8 +20,8 @@ function selectionSort(arr) {
   //Implementation for selectionSort
   for (let i = 0; i < arr.length; i++) {
     let indexofMin = i;
-    for (let j = i + 1; arr.length; j++) {
-      if (arr[j] < indexofMin) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexofMin]) {
         indexofMin = j;
       }
     }
@@ -36,11 +36,27 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) return arr;
 
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
+  const results = [];
 
+  while(left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
